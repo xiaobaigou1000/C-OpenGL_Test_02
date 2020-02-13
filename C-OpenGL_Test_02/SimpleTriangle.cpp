28b375b593e06436f836db894347bad5ee54657d
xiaobaigou1000 <xiaobaigou2000@qq.com>
2020-02-14 00:51:47 +0800

    simple ebo

--- C-OpenGL_Test_02/SimpleTriangle.cpp
+++ C-OpenGL_Test_02/SimpleTriangle.cpp
@@ -7,23 +7,29 @@ SimpleTriangle::SimpleTriangle()
 void SimpleTriangle::initTriangle()
 {
     QOpenGLFunctions_4_5_Core::initializeOpenGLFunctions();
-    glCreateBuffers(1, &triangleVBO);
-    glNamedBufferData(triangleVBO, triangleVertices.size() * sizeof(float), triangleVertices.data(), GL_STATIC_DRAW);
-    glCreateVertexArrays(1, &triangleVAO);
-    glBindVertexArray(triangleVAO);
-    glBindBuffer(GL_ARRAY_BUFFER, triangleVBO);
+
+    glCreateBuffers(1, &VBO);
+    glNamedBufferData(VBO, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
+    glCreateBuffers(1, &EBO);
+    glNamedBufferData(EBO, indices.size() * sizeof(float), indices.data(), GL_STATIC_DRAW);
+    
+    glCreateVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
     glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+
     triangleShader.create();
     triangleShader.addShaderFromSourceFile(QOpenGLShader::Vertex, "triangleVertexShader.vert");
     triangleShader.addShaderFromSourceFile(QOpenGLShader::Fragment, "triangleFragmentShader.frag");
     triangleShader.link();
-    triangleShader.bind();
 }
 
 void SimpleTriangle::drawTriangle()
 {
-    glBindVertexArray(triangleVAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glBindVertexArray(VAO);
+    triangleShader.bind();
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)0);
 }
 
--- C-OpenGL_Test_02/SimpleTriangle.h
+++ C-OpenGL_Test_02/SimpleTriangle.h
@@ -7,16 +7,23 @@ class SimpleTriangle :public QOpenGLFunctions_4_5_Core
 {
 public:
     SimpleTriangle();
-    unsigned int triangleVAO, triangleVBO, triangleEBO;
+    unsigned int VAO, VBO, EBO;
     QOpenGLShaderProgram triangleShader;
     void initTriangle();
     void drawTriangle();
 
-    constexpr static std::array<float, 9> triangleVertices =
+    constexpr static std::array<float, 12> vertices =
     {
+        0.5f, 0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
         -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f
+        -0.5f, 0.5f, 0.0f
+    };
+
+    constexpr static std::array<unsigned int, 6> indices =
+    {
+        0, 1, 3,
+        1, 2, 3
     };
 };
 
